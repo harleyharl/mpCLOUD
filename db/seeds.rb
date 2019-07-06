e913ce57b21730969379a86fac4ad39cc5b9c6a5
harleyharl <harleymteden@gmail.com>
2019-07-06 10:05:00 -0700

    seeded a user, some soundkits and a couple of recordings

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,10 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+User.create({username: 'Harley', email_address: 'harleymteden@gmail.com', password: 'harley'})
+
+Soundkit.create([{ name: 'Star Wars Drums', url: 'some website' }, { url: 'some site', name: 'Splice Sounds v1' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Recording.create([{title: 'cool beat', user_id: 1}, {title: 'cool beat 2', user_id: 1}])
+
+UserSoundkit.create({user_id: 1, soundkit_id: 1})
