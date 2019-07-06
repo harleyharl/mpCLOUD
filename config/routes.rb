ff6d686da3c67e8b6c655f028e355d4982ae62a5
harleyharl <harleymteden@gmail.com>
2019-07-06 10:00:07 -0700

    set up rails routes under /api scope for soundkits and recordings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :soundkits
+  # resources :recordings
+  # resources :users
+  scope '/api' do
+    get :soundkits, to: 'soundkits#index'
+    get :recordings, to: 'recordings#index'
+  end
 end
