8dc7fd71c5e0b70973ec77ec07cb859cc662aece
harleyharl <harleymteden@gmail.com>
2019-07-07 21:37:09 -0700

    build out pads container and started building renderPads method. Work in progress

---
+++ client/src/components/Pads.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Pad from './Pad'
+class Pads extends Component {
+  // renderPads = () => this.props.sounds.currentSounds.map((sound) => <Pad sound={sound} key={sound.id} url={sound.url} name={sound.name} />)
+
+  render() {
+    // debugger
+    return(
+      <ul>
+      </ul>
+    );
+  }
+};
+
+export default Pads;
