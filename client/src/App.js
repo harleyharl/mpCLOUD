42b348617bdea535f1b477ad74e06710da74c86c
harleyharl <harleymteden@gmail.com>
2019-07-07 21:34:37 -0700

    built out wrapping container component, set up to render container components and connected to the store.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PadContainer from './containers/PadContainer'
+import SoundKitContainer from './containers/SoundKitContainer'
+import { connect } from 'react-redux'
+import { fetchSoundkits } from './actions/samplerActions'
+import { fetchSounds } from './actions/samplerActions'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        MPCLOUD 1.0!
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      user: '',
+      soundKits: [],
+      currentSoundkit: [],
+      recordings: []
+    }
+  }
+
+  componentDidMount(){
+    const soundKits = this.props.fetchSoundkits()
+    this.setState({
+      soundKits: soundKits,
+    })
+  }
+
+  // renderPadContainer = () => {
+  //   <PadContainer currentSoundkit={this.props.currentSoundkit}/>
+  // }
+
+  handleOnChange = event => {
+    this.setState({
+      currentSoundkit: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SoundKitContainer onChange={this.handleOnChange} soundKits={this.props.soundKits}/>
+        <PadContainer currentSoundkit={this.props.currentSoundkit}/>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+const mapStateToProps = state => ({
+  soundKits: state.soundKits,
+  currentSoundkit: state.currentSoundkit
+})
+
+export default connect(mapStateToProps, {fetchSoundkits, fetchSounds})(App);
