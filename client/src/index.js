0095bed1ce0b0a612961de9b867e5c7aab92b7c7
harleyharl <harleymteden@gmail.com>
2019-07-07 21:41:20 -0700

    added redux devTools for easier debugging!

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers' //look at reducers, not really doing anything now..
 import * as serviceWorker from './serviceWorker';
 // figure out how to add redux devtools
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
