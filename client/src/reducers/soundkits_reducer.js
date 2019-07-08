a205662668375ef2703103077aa68983842ac46b
harleyharl <harleymteden@gmail.com>
2019-07-07 21:45:00 -0700

    built action switch to handle fetch requests and setting the current soundkit

---
+++ client/src/reducers/soundkits_reducer.js
@@ -0,0 +1,25 @@
+export default function soundkitsReducer(state = {
+  loading: false,
+  soundKits: [],
+  currentSoundkit: null
+}, action) {
+
+  switch (action.type) {
+
+    case 'LOADING_SOUNDKITS':
+    return {...state, loading:true}
+
+    case 'FETCH_SOUNDKITS':
+    return { ...state, soundKits: action.payload, loading: false }
+
+    case 'LOADING_SOUNDS':
+    return {...state, loading:true}
+
+    case 'SET_CURRENT_SOUNDKIT':
+    debugger
+    return { ...state, currentSoundkit: action.payload, loading: false }
+
+  default:
+    return state;
+  };
+}
