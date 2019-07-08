4cc4d5b45d6be0e604476c1ead93b082046162e0
harleyharl <harleymteden@gmail.com>
2019-07-07 21:35:58 -0700

    built actions for fetching soundkits(so a user can select from a list of soundkits), built an action for fetching a single soundkit using a soundkit's id as a parameter, and an action to set a current soundkit to be used inside the pads container

---
+++ client/src/actions/samplerActions.js
@@ -0,0 +1,24 @@
+export function fetchSoundkits() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_SOUNDKITS' });
+    return fetch('/api/soundkits')
+      .then(response => response.json())
+      .then(json => dispatch({ type: 'FETCH_SOUNDKITS', payload: json}));
+  };
+}
+
+export function fetchSounds(soundkit_id) {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_SOUNDS' });
+    return fetch(`/api/soundkits/${soundkit_id}`)
+      .then(response => response.json())
+      .then(json => dispatch({ type: 'FETCH_SOUNDS', payload: json}));
+  };
+}
+
+export function setCurrentSoundkit(soundkit) {
+  // soundkit comes in here undefined
+  return (dispatch) => {
+    dispatch({ type: 'SET_CURRENT_SOUNDKIT', payload: soundkit })
+  };
+}
