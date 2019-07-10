c53097d9316899c55158d4a860ce9ea09e4bdf87
harleyharl <harleymteden@gmail.com>
2019-07-10 13:38:26 -0700

    built renderPads method which passes down the correct props to the Pad component

--- client/src/components/Pads.js
+++ client/src/components/Pads.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Pad from './Pad'
 class Pads extends Component {
-  // renderPads = () => this.props.sounds.currentSounds.map((sound) => <Pad sound={sound} key={sound.id} url={sound.url} name={sound.name} />)
+  renderPads = () => this.props.sounds.map((sound) => <Pad sound={sound} key={sound.id} url={sound.url} name={sound.name} />)
 
   render() {
-    // debugger
     return(
       <ul>
+        {this.renderPads()}
       </ul>
     );
   }
