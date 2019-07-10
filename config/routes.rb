4db5b5e2d1c841f90cf209e8e7494cadd2cd8c56
harleyharl <harleymteden@gmail.com>
2019-07-10 13:41:26 -0700

    added comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   scope '/api' do
     get :sounds, to: 'sounds#index'
+    # get ':soundkits/:id/soundfiles', to: 'soundkits#getsoundfiles'
     get :soundkits, to: 'soundkits#index'
     get :recordings, to: 'recordings#index'
     get ':soundkits/:id', to: 'soundkits#show'
