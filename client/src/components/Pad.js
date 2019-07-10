809d9d9ea49dfa0dadaa231f0e011596eef81ca4
harleyharl <harleymteden@gmail.com>
2019-07-10 13:37:42 -0700

    imported react-sound and built buttons to trigger sounds upon click. implemented react-sound functionality

--- client/src/components/Pad.js
+++ client/src/components/Pad.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import Sound from 'react-sound'
 
 class Pad extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      playStatus: 'NOT PLAYING'
+    }
+  }
+
+  play = () => {
+    this.setState({
+      playStatus: 'PLAYING'
+    })
+  }
+
   render() {
+    const audio = new Audio('./public/Snap.wav')
     return (
       <div>
-        {this.props.name}<br/>
-        <a href={this.props.url}>{this.props.url}</a>
+        <button onClick={this.play} class="pad" type="button" key={this.props.id} src={this.props.url}>
+          <p className="padText">{this.props.name}</p>
+          <Sound playStatus={this.state.playStatus} url={'https://mpcloud.s3-us-west-1.amazonaws.com/MM_-_METAL_SNARE_-_C%23M.wav'} />
+        </button>
+
+        <button onClick={audio.play()}>
+        </button>
+
       </div>
     );
   }
