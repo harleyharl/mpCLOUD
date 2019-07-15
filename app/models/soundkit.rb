6c4effea54717aa4dfdaaa2c71489d76a0d48b77
harleyharl <harleymteden@gmail.com>
2019-07-15 09:20:26 -0700

    removed has_many_attached_sounds because it was making my app break

--- app/models/soundkit.rb
+++ app/models/soundkit.rb
@@ -2,5 +2,5 @@ class Soundkit < ApplicationRecord
   has_many :user_soundkits
   has_many :users, through: :user_soundkits
   has_many :sounds
-  has_many_attached :sounds
+  # has_many_attached :sounds   WHY DOES THIS BREAK MY APP?
 end
