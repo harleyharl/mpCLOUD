9c2be2dacad7e88a43ee22daff2d62c6b4e6718e
harleyharl <harleymteden@gmail.com>
2019-07-15 21:24:27 -0700

    built a NotFound class component for when users enter a bad url

---
+++ client/src/components/Soundkit/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+
+  render() {
+    return (
+      <div>That route is not available</div>
+    );
+  }
+
+}
+
+export default NotFound;
