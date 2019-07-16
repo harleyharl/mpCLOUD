860f5b5e786eccb04fef6ed4e65f7c3e7a104707
harleyharl <harleymteden@gmail.com>
2019-07-15 21:14:26 -0700

    added soundkit_params with nested attributes and a create action

--- app/controllers/soundkits_controller.rb
+++ app/controllers/soundkits_controller.rb
@@ -5,12 +5,6 @@ class SoundkitsController < ApplicationController
   end
 
   def show
-
-    # id = params[:id]
-    # @soundkit = Soundkit.where(id:id)
-    #
-    # @soundkit.attach(io: File.open('../client/src/public'), filename: sound.name)
-
     id = params[:id]
     render(
       status: 200,
@@ -18,21 +12,18 @@ class SoundkitsController < ApplicationController
     )
   end
 
+  def create
+    # Rails.application.routes.url_helpers.rails_blob_path(Soundkit.all.last.sounds.first.sound_file_attachment, only_path: true)
+    @soundkit = Soundkit.new(soundkit_params)
+    @soundkit.sounds.each do |sound|
+      sound.url = url_for(sound.sound_file)
+      sound.name = sound.sound_file.name
+    end
+    @soundkit.save
+  end
+
+  def soundkit_params
+    params.require(:soundkit).permit([:name, :description, sounds_attributes: %I[id name sound_file _destroy]])
+  end
+
 end
-
-def create
-end 
-
-# filter example
-  # def index
-  #   q = params[:q]
-  #
-  #   if q.blank?
-  #     render status: 400, json: { error: 'Expected parameter `q` '}
-  #   else
-  #     render(
-  #       status: 200,
-  #       json: Food.where(["description LIKE ?", "%#{q}%"]).limit(100)
-  #     )
-  #   end
-  # end
