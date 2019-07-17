4649288f6dea03a72bed3da0c645e0951b528274
harleyharl <harleymteden@gmail.com>
2019-07-16 20:27:14 -0700

    removed soundsReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import soundkitsReducer from './soundkits_reducer'
-import soundsReducer from './sounds_reducer'
 
 const rootReducer = combineReducers({
   soundKits: soundkitsReducer,
-  currentSounds: soundsReducer 
 });
 
 export default rootReducer;
