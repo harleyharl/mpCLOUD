a6384454e37bbf56c1b1cee641b351464b2d137b
harleyharl <harleymteden@gmail.com>
2019-07-16 20:26:21 -0700

    removed redundant code, updated imported components names and simplified ReactDOM.render method (moved Provider method to Root component)

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import Routes from './Routes'
-import { Provider } from 'react-redux'
+// import App from './App';
+import Root from './components/Root'
+// import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers' //look at reducers, not really doing anything now..
@@ -14,12 +14,7 @@ const store = createStore(rootReducer, compose(applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ))
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
