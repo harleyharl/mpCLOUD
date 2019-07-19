1e80dec937fb885b8f50891da7ccb58b2a3478f6
harleyharl <harleymteden@gmail.com>
2019-07-19 11:58:45 -0700

    re-arranged root component to make routes work properly as well as the NotFound component

---
+++ client/src/Root.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { Provider } from 'react-redux'
+import App from './App'
+import SoundkitNew from './components/Soundkit/New';
+import SoundkitEdit from './components/Soundkit/Edit';
+import NotFound from './components/Soundkit/NotFound';
+
+const history = createBrowserHistory();
+
+const Root = ({ store }) =>
+<Provider store={store}>
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route path="/new" component={SoundkitNew} />
+      <Route path="/soundkits/:id/edit" component={SoundkitEdit} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+</Provider>
+
+export default Root;
