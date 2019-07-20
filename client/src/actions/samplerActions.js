07a88c54f2effbfb92c293ddd797337ac5120b45
harleyharl <harleymteden@gmail.com>
2019-07-20 10:16:35 -0700

    added removeSoundkit action to handle deletion of a particular soundkit

--- client/src/actions/samplerActions.js
+++ client/src/actions/samplerActions.js
@@ -21,3 +21,45 @@ export function setCurrentSoundkit(soundkit) {
     dispatch({ type: 'SET_CURRENT_SOUNDKIT', payload: soundkit })
   };
 }
+
+// export function removeSoundkit(soundkitId) {
+//   return (dispatch) => {
+//     debugger
+//     dispatch({ type: 'REMOVE_SOUNDKIT' })
+//     return fetch(`/api/soundkits/${soundkit_id}`)
+//       .then(response => response.json())
+//       .then(json => dispatch({ type: 'REMOVE_SOUNDKIT', payload: json}));
+//   }
+// }
+
+
+export function removeSoundkit(soundkitId, data={}) {
+  return (dispatch) => {
+    return fetch(`/api/soundkits/${soundkitId}`, {
+        method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+      
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+    .then(response => response.json()) // parses JSON response into native JavaScript objects
+    .then(json => dispatch({ type: 'REMOVE_SOUNDKIT', payload: json}))
+  }
+}
+
+
+// function postData(url = '', data = {}) {
+//   // Default options are marked with *
+//     return fetch(url, {
+//         method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//         mode: 'cors', // no-cors, cors, *same-origin
+//         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//         credentials: 'same-origin', // include, *same-origin, omit
+//         headers: {
+//             'Content-Type': 'application/json',
+//             // 'Content-Type': 'application/x-www-form-urlencoded',
+//         },
+//         redirect: 'follow', // manual, *follow, error
+//         referrer: 'no-referrer', // no-referrer, *client
+//         body: JSON.stringify(data), // body data type must match "Content-Type" header
+//     })
+//     .then(response => response.json()); // parses JSON response into native JavaScript objects
+// }
