26ad4cce8cb8b90a3fcb466086b08f298118924c
harleyharl <harleymteden@gmail.com>
2019-07-22 17:03:51 -0700

    implemented individual sounds having their own names based on their file names

--- app/controllers/soundkits_controller.rb
+++ app/controllers/soundkits_controller.rb
@@ -19,11 +19,10 @@ class SoundkitsController < ApplicationController
   end
 
   def create
-    # Rails.application.routes.url_helpers.rails_blob_path(Soundkit.all.last.sounds.first.sound_file_attachment, only_path: true)
     @soundkit = Soundkit.new(soundkit_params)
     @soundkit.sounds.each do |sound|
       sound.url = url_for(sound.sound_file)
-      sound.name = sound.sound_file.name
+      # sound.name = sound.sound_file.name
     end
     @soundkit.save
   end
