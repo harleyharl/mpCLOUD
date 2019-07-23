f95a5bb44408332dd6300ec48bf8b43b27fd1348
harleyharl <harleymteden@gmail.com>
2019-07-22 17:08:23 -0700

    tidying

--- client/src/components/Pads/Pads.js
+++ client/src/components/Pads/Pads.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Pad from './Pad'
 class Pads extends Component {
-  
+
   renderPads = () => this.props.sounds.map((sound) => <Pad sound={sound} key={sound.id} url={sound.url} name={sound.name} />)
 
   render() {
