7dd10f31bbc4b0006591b6938e85b5fedd9b77c1
harleyharl <harleymteden@gmail.com>
2019-07-27 09:48:22 -0700

    added styling for individual drum pads with styled-components. added audio tag with an onPlay event handler to send the current sound to the Vizualizer. added event listener for keyboard to control pads

--- client/src/components/Pads/Pad.js
+++ client/src/components/Pads/Pad.js
@@ -1,32 +1,77 @@
 import React, { Component } from 'react';
 import Sound from 'react-sound'
+import styled from 'styled-components'
+import { setCurrentSound } from '../../actions/samplerActions'
+import { connect } from 'react-redux'
+
+
+const Button = styled.button`
+  margin: 10px;
+  font-family: Times;
+  background-color: DimGrey;
+  height:10em;
+  width:10em;
+  position: relative
+  border-radius: 20px;
+  overflow: hidden
+`
+
+const ButtonText = styled.p`
+  background-color: DimGrey;
+  font-family: Arial
+  font-size: 15px
+  line-height: 15px
+  width: 45px
+  color: white
+  position: absolute;
+  bottom: -5px;
+  right: 10px;
+  border: 0;
+  overflow: hidden
+  text-align: right
+  word-wrap: break-word;
+`
 
 class Pad extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      playStatus: 'STOPPED'
-    }
+  sendToVizualizer = (e) => {
+    debugger
+    this.props.setCurrentSound(e.currentTarget.src)
   }
 
-  play = () => {
-    this.setState({
-      playStatus: 'PLAYING'
-    })
+  handleClick = () => {
+    this.audio.play()
+    this.audio.currentTime = 0
+  }
+
+  componentDidMount(){
+    document.addEventListener('keypress', this.handleKeyDown)
+  }
+
+  handleKeyDown = (e) => {
+    if (e.charCode === this.props.letter.charCodeAt()) {
+    this.audio.play()
+    this.audio.currentTime = 0
+    }
   }
 
   render() {
     return (
-      <div>
-        <button onClick={this.play} className="pad" type="button" key={this.props.id} src={this.props.url}>
-          <p className="padText">{this.props.name}</p>
-          <Sound playStatus={this.state.playStatus} url={this.props.url} />
-        </button>
-      </div>
+        <Button type="button" id={this.props.sound.id} onClick={this.handleClick}>
+        <ButtonText> {this.props.name.toLowerCase()} </ButtonText>
+          <audio
+            className="pad"
+            ref={ref => this.audio = ref}
+            className='clip'
+            src={this.props.url}
+            id={this.props.id}
+            onPlay={e => this.sendToVizualizer(e)}
+            >
+          </audio>
+        </Button>
     );
   }
 
 }
 
-export default Pad;
+export default connect(null, {setCurrentSound})(Pad);
