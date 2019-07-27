e41911d97248b4c9e15f9052a7b1317d3749a56f
harleyharl <harleymteden@gmail.com>
2019-07-27 09:50:00 -0700

    gave each pad a 'letter' prop to be passed down so each pad can be played with the keyboard as well as the mouse. added react bootstrap element for container

--- client/src/components/Pads/Pads.js
+++ client/src/components/Pads/Pads.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
 import Pad from './Pad'
+import Container from 'react-bootstrap/Container'
+
 class Pads extends Component {
 
-  renderPads = () => this.props.sounds.map((sound) => <Pad sound={sound} key={sound.id} url={sound.url} name={sound.name} />)
+  renderPads = () => this.props.sounds.map((sound, index) => <Pad index={index} sound={sound} key={sound.id} url={sound.url} name={sound.name} letter={(this.assignLetter(index))} />)
+
+
+  assignLetter = (index) => {
+    if (index === 0) {
+        return 'a'
+    } else if (index === 1) {
+        return 'w'
+    } else if (index === 2){
+        return 's'
+    } else if (index === 3){
+        return 'e'
+    } else if (index === 4){
+        return 'd'
+    } else if (index === 5){
+        return 'f'
+    } else if (index === 6){
+        return 't'
+    } else if (index === 7){
+        return 'g'
+    } else if (index === 8){
+        return 'y'
+    } else if (index === 9){
+        return 'h'
+    } else if (index === 10){
+        return 'u'
+    } else if (index === 11){
+        return 'j'
+    } else if (index === 12){
+        return 'i'
+    } else if (index === 13){
+        return 'k'
+    } else if (index === 14){
+        return 'o'
+    } else if (index === 15){
+        return 'l'
+    }
+  }
 
   render() {
     return(
-      <ul>
+      <Container >
         {this.renderPads()}
-      </ul>
+      </Container >
     );
   }
 };
