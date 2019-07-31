ee8b8a973e3aaef7c229dc2c3f7b17ed45489277
harleyharl <harleymteden@gmail.com>
2019-07-30 19:51:48 -0700

    passed analyser down as a prop to Pads component

--- client/src/containers/PadContainer.js
+++ client/src/containers/PadContainer.js
@@ -9,7 +9,7 @@ class PadContainer extends Component {
   render() {
     return (
       <Container>
-        <Pads context={this.props.context} sounds={this.props.currentSoundkit.sounds}/>
+        <Pads analyser={this.props.analyser} context={this.props.context} sounds={this.props.currentSoundkit.sounds}/>
         <div>{this.props.currentSoundkit.description}</div>
       </Container>
     );
