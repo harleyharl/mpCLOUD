d9cf2f6d99d573ef62d73c342615c9b513c2c5d9
harleyharl <harleymteden@gmail.com>
2019-08-05 10:26:41 -0700

    deleted redundant code

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -3,18 +3,4 @@ class SoundsController < ApplicationController
     @sounds = Sound.all
     render json: @sounds, status: 200
   end
-
 end
-
-# def index
-#   q = params[:q]
-#
-#   if q.blank?
-#     render status: 400, json: { error: 'Expected parameter `q` '}
-#   else
-#     render(
-#       status: 200,
-#       json: Food.where(["description LIKE ?", "%#{q}%"]).limit(100)
-#     )
-#   end
-# end
