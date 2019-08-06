dc25c9cdc338a0466f58ce3cb92291b5d29afed9
harleyharl <harleymteden@gmail.com>
2019-08-05 17:56:40 -0700

    added some react-bootstrap stylign

--- client/src/components/Soundkit/New/index.js
+++ client/src/components/Soundkit/New/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import SoundkitForm from '../Form/index';
+import Container from 'react-bootstrap/Container'
 
 class SoundkitNew extends Component {
   render() {
     return (
-      <div className="SoundkitNew col-md-8 col-md-offset-2">
-        <h2>New Soundkit</h2>
+      <div className="SoundkitNew">
+      <Container>
+        <h2>Build Your Own Soundkit</h2>
         <SoundkitForm history={this.props.history} match={this.props.match} />
+      </Container>
       </div>
     );
   }
