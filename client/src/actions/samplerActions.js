4dec7ff45c319530e6d9909dba93a64d72bf99b1
harleyharl <harleymteden@gmail.com>
2019-08-05 17:50:35 -0700

    removed redundant code

--- client/src/actions/samplerActions.js
+++ client/src/actions/samplerActions.js
@@ -22,25 +22,13 @@ export function setCurrentSoundkit(soundkit) {
   };
 }
 
-// export function removeSoundkit(soundkitId) {
-//   return (dispatch) => {
-//     debugger
-//     dispatch({ type: 'REMOVE_SOUNDKIT' })
-//     return fetch(`/api/soundkits/${soundkit_id}`)
-//       .then(response => response.json())
-//       .then(json => dispatch({ type: 'REMOVE_SOUNDKIT', payload: json}));
-//   }
-// }
-
-
 export function removeSoundkit(soundkitId, data={}) {
   return (dispatch) => {
     return fetch(`/api/soundkits/${soundkitId}`, {
-        method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
-
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
+        method: 'DELETE',
+        body: JSON.stringify(data),
     })
-    .then(response => response.json()) // parses JSON response into native JavaScript objects
+    .then(response => response.json()) 
     .then(json => dispatch({ type: 'REMOVE_SOUNDKIT', payload: json}))
   }
 }
@@ -56,22 +44,3 @@ export function setCurrentSound(soundUrl) {
     dispatch({ type: 'SET_CURRENT_SOUND', soundUrl })
   };
 }
-
-
-// function postData(url = '', data = {}) {
-//   // Default options are marked with *
-//     return fetch(url, {
-//         method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//         mode: 'cors', // no-cors, cors, *same-origin
-//         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//         credentials: 'same-origin', // include, *same-origin, omit
-//         headers: {
-//             'Content-Type': 'application/json',
-//             // 'Content-Type': 'application/x-www-form-urlencoded',
-//         },
-//         redirect: 'follow', // manual, *follow, error
-//         referrer: 'no-referrer', // no-referrer, *client
-//         body: JSON.stringify(data), // body data type must match "Content-Type" header
-//     })
-//     .then(response => response.json()); // parses JSON response into native JavaScript objects
-// }
