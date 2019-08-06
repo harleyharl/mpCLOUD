590bbee9e19105ff3fbd824e9aeffd1205fa6f0b
harleyharl <harleymteden@gmail.com>
2019-08-05 17:42:08 -0700

    removed redundant code

--- app/models/soundkit.rb
+++ app/models/soundkit.rb
@@ -1,12 +1,9 @@
 class Soundkit < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
-  # has_many :user_soundkits
-  # has_many :users, through: :user_soundkits
 
   has_many :sounds, dependent: :destroy
   accepts_nested_attributes_for :sounds, allow_destroy: true
-  # has_many_attached :sounds
 
   def as_json(_opts = {})
     {
