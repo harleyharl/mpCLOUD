3909581c5ad181ac941225adff02b65a3b6ce8be
harleyharl <harleymteden@gmail.com>
2019-08-08 13:09:28 -0700

    added navbar to new soundkit page

--- client/src/components/Soundkit/New/index.js
+++ client/src/components/Soundkit/New/index.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import SoundkitForm from '../Form/index';
 import Container from 'react-bootstrap/Container'
 import './index.css'
+import NavBar from '../../NavBar'
+
 
 class SoundkitNew extends Component {
   render() {
     return (
-      <div className="SoundkitNew">
-      <Container>
-        <h2>Build Your Own Soundkit</h2>
-        <SoundkitForm history={this.props.history} match={this.props.match} />
-      </Container>
+      <div>
+        <NavBar history={this.props.history}/>
+        <div className="SoundkitNew">
+        <Container>
+          <h2>Build Your Own Soundkit</h2>
+          <SoundkitForm history={this.props.history} match={this.props.match} />
+        </Container>
+        </div>
       </div>
     );
   }
