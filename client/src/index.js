c133bbb7c884f990049d71f9d6e171f53c9938c4
harleyharl <harleymteden@gmail.com>
2019-08-08 13:10:52 -0700

    removed redundant code and comments

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
 import Root from './Root'
-// import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
-import rootReducer from './reducers' //look at reducers, not really doing anything now..
+import rootReducer from './reducers'
 import * as serviceWorker from './serviceWorker';
-// figure out how to add redux devtools
-// const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -16,7 +12,4 @@ const store = createStore(rootReducer, compose(applyMiddleware(thunk),
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
