9a0c85fd61fa0e122b268eedde6bbca3bb9ff572
harleyharl <harleymteden@gmail.com>
2019-08-12 21:36:30 -0700

    removed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,27 +10,3 @@
 # Recording.create([{title: 'cool beat', user_id: 1}, {title: 'cool beat 2', user_id: 1}])
 #
 # UserSoundkit.create({user_id: 1, soundkit_id: 1})
-
-
-Soundkit.create([
-  { name: 'Medasin Microdose Vol. 1', url: 'amazon s3'},
-  ])
-
-Sound.create([
-  { name: 'Chunk Snare', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/MM_-_CHUNK_SNARE_2.wav', soundkit_id: 1},
-  { name: 'Coins', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/CoinsMovement_S08FO.749.wav', soundkit_id: 1},
-  { name: 'Smoothie', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/FD_85_Emaj9_Smoothie_FA.wav', soundkit_id: 1},
-  { name: 'Rhodes', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/G%23maj7_Rhodes_High_TL.wav', soundkit_id: 1},
-  { name: 'Flange Hat', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/HH_Flange2.wav', soundkit_id: 1},
-  { name: 'Purple Hat', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/HHr_Purple.wav', soundkit_id: 1},
-  { name: 'Hydrophone', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/Hydrophone_mic_-_water_in_glass.wav', soundkit_id: 1},
-  { name: 'Tremolo Organ', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/JNTHN_organ_tremolo_03_Ebm9.wav', soundkit_id: 1},
-  { name: 'QQ Snare', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/MEDASIN_QQ_snare_12.wav', soundkit_id: 1},
-  { name: 'Kick', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/MM_-_ANOTHER_KICK_-_FM.wav', soundkit_id: 1},
-  { name: 'Beep Synth', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/MM_-_BEEP_SYNTH_-_C%23M.wav', soundkit_id: 1},
-  { name: 'Hey Now Clap', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/MM_-_HEYNOW2CLAP_2.wav', soundkit_id: 1},
-  { name: 'Metallic Clap', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/MM_-_METALLIC_CLAP_-_G%23M.wav', soundkit_id: 1},
-  { name: 'Metal Snare', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/MM_-_METAL_SNARE_-_C%23M.wav', soundkit_id: 1},
-  { name: 'Kerri Organ', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/MRDH_-_CS_Kerri_Organ_Fm.wav', soundkit_id: 1},
-  { name: 'Reverse Trench', url: 'https://mpcloud.s3-us-west-1.amazonaws.com/Medasin+Microdose+Vol.+1/NL1_Reverse_Trench_Fm.wav', soundkit_id: 1}
-  ])
