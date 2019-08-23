88821cc6a1085ebe9d06a7462b8bee670ab782b3
harleyharl <harleymteden@gmail.com>
2019-08-23 12:04:33 -0700

    added actions for adding and editing soundkits in the redux store

--- client/src/reducers/soundkits_reducer.js
+++ client/src/reducers/soundkits_reducer.js
@@ -8,13 +8,13 @@ export default function soundkitsReducer(state = {
   switch (action.type) {
 
     case 'LOADING_SOUNDKITS':
-    return {...state, loading:true}
+    return {...state, loading: true}
 
     case 'FETCH_SOUNDKITS':
     return { ...state, soundKits: action.payload, loading: false }
 
     case 'LOADING_SOUNDS':
-    return {...state, loading:true}
+    return {...state, loading: true}
 
     case 'SET_CURRENT_SOUNDKIT':
     return { ...state, currentSoundkit: action.payload, loading: false }
@@ -23,10 +23,31 @@ export default function soundkitsReducer(state = {
     return {soundKits: state.soundKits.filter(soundKit => soundKit.id !== action.payload.id)}
 
     case 'CLEAR_CURRENT_SOUNDKIT':
-    return {...state, currentSoundkit: null, loading:false}
+    return {...state, currentSoundkit: null}
 
     case 'SET_CURRENT_SOUND':
     return { ...state, currentSound: action.soundUrl}
+
+    case 'ADDING_SOUNDKIT':
+    return {...state, loading: true }
+
+    case 'ADD_NEW_SOUNDKIT':
+    return {...state, soundKits: [...state.soundKits, action.payload], loading: false}
+
+    case 'EDITING_SOUNDKIT':
+    return {...state, loading: true}
+
+    case 'EDITED_SOUNDKIT':
+    // action.payload was an array because i had used .where in my controller action!!! duh!!!
+    return {...state, loading: false, soundKits: state.soundKits.map((soundkit, index) => {
+      if (soundkit.id === action.payload[0].id) {
+        debugger
+        return action.payload[0]
+      } else {
+        return soundkit
+      }
+    })
+  }
 
 
   default:
