83d7e7eb294a2a7731470163af3a13c3ebc1ea1e
harleyharl <harleymteden@gmail.com>
2021-02-16 15:58:16 -0800

    removed api before routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
 
   #TODO do i need to delete the scope here? heroku
 
-  scope '/api' do
-    get :sounds, to: 'sounds#index'
-    # get ':soundkits/:id/soundfiles', to: 'soundkits#getsoundfiles'
-    get :soundkits, to: 'soundkits#index'
-    get :recordings, to: 'recordings#index'
-    get ':soundkits/:id', to: 'soundkits#show'
-    patch ':soundkits/:id', to: 'soundkits#update'
-    post :soundkits, to: 'soundkits#create'
-    delete ':soundkits/:id', to: 'soundkits#destroy'
-  end
+  get :sounds, to: 'sounds#index'
+  # get ':soundkits/:id/soundfiles', to: 'soundkits#getsoundfiles'
+  get :soundkits, to: 'soundkits#index'
+  get :recordings, to: 'recordings#index'
+  get ':soundkits/:id', to: 'soundkits#show'
+  patch ':soundkits/:id', to: 'soundkits#update'
+  post :soundkits, to: 'soundkits#create'
+  delete ':soundkits/:id', to: 'soundkits#destroy'
 
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
