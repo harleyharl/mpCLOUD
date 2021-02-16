c04d32e9d5cbddbd1b14e3afc4b7da5f27463fb0
harleyharl <harleymteden@gmail.com>
2021-02-16 12:10:16 -0800

    added google cloud as the active storage db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon
+
+  #use config.active_storage.service = :google_dev if i want to deploy on google cloud 
+  config.active_storage.service = :google
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
