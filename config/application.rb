f0cdc33fdff47f3996188a7aed03e82f9af26f60
harleyharl <harleymteden@gmail.com>
2021-02-16 15:36:49 -0800

    tried removing api_only config

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module MpCLOUDApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # config.api_only = true
 
 
     # allow the whilelisted domain to send request across. change to localhost to make private
