b36666b60abde871f783cd7760a814842e76887b
harleyharl <harleymteden@gmail.com>
2021-02-16 17:41:37 -0800

    tried adding api back to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
 
   #TODO do i need to delete the scope here? heroku
 
-  get :sounds, to: 'sounds#index'
-  # get ':soundkits/:id/soundfiles', to: 'soundkits#getsoundfiles'
-  get :soundkits, to: 'soundkits#index'
-  get :recordings, to: 'recordings#index'
-  get ':soundkits/:id', to: 'soundkits#show'
-  patch ':soundkits/:id', to: 'soundkits#update'
-  post :soundkits, to: 'soundkits#create'
-  delete ':soundkits/:id', to: 'soundkits#destroy'
+  scope '/api' do
+    get :sounds, to: 'sounds#index'
+    # get ':soundkits/:id/soundfiles', to: 'soundkits#getsoundfiles'
+    get :soundkits, to: 'soundkits#index'
+    get :recordings, to: 'recordings#index'
+    get ':soundkits/:id', to: 'soundkits#show'
+    patch ':soundkits/:id', to: 'soundkits#update'
+    post :soundkits, to: 'soundkits#create'
+    delete ':soundkits/:id', to: 'soundkits#destroy'
+  end
 
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
